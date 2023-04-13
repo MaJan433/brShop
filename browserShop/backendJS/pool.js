24d7f76d3ceeaf363ce6f7ded48e62b39c615c94
MaJan433 <69705881+MaJan433@users.noreply.github.com>
2023-04-13 07:17:41 +0200

    Update pool.js

--- browserShop/backendJS/pool.js
+++ browserShop/backendJS/pool.js
@@ -2,10 +2,10 @@ const mysql2 = require('mysql2/promise');
 
 const pool = mysql2.createPool({
     host: 'localhost',
-    user: 'root',
-    database: 'portfolio_shop',
-    password: 'maria11',
-    port: 4900,
+    user: 'user',
+    database: 'database',
+    password: 'password',
+    port: port,
     namedPlaceholders: true
 });
 module.exports = {
